7df808ed00b47867050919bec9b439b91a639309
Matt Luna <mat.c.luna@gmail.com>
2021-11-26 16:25:50 -0600

    finished feature/mutation

--- server/server.js
+++ server/server.js
@@ -9,11 +9,14 @@ const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const { authMiddleware } = require("./utils/auth");
+
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware
   });
 
   // Start the Apollo server
@@ -25,7 +28,7 @@ const startServer = async () => {
   // log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
-
+ 
 // Initialize the Apollo server
 startServer();
 
