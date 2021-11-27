df1b0d21c65e9597a01272f71ea61b6d950a234c
Matt Luna <mat.c.luna@gmail.com>
2021-11-26 23:04:11 -0600

    root level

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
+const path = require('path');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const { authMiddleware } = require("./utils/auth");
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
@@ -34,6 +34,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
